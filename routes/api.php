040bc9ac340205ed68dcd0d4bd705632c9ca890b
johnasblasco <johnaslblasco@gmail.com>
2025-02-28 18:18:13 +0800

    CRUD API DONE

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+Route::get('/', function () {
+    return [
+        [
+            "hehe" => "huhu",
+            "haha" => "hoho"
+        ]
+    ];
+});
+
+// Routes
+Route::apiResource('post', PostController::class);
